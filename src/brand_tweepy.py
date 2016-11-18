3636ec3f84059d16a20ae77d5cb35db5b2c6539f
rarezhang <catezhang@qq.com>
2016-11-18 17:35:33 +0000

    remove credentials

--- src/brand_tweepy.py
+++ src/brand_tweepy.py
@@ -5,19 +5,8 @@ Brand Advertising
 from urllib.request import urlopen
 import tweepy, re, json
 
+from twitter_auth import consumer_key, consumer_secret, access_token, access_token_secret
 
-# user credentials 
-# cate.wl.zhang@gmail.com
-# Consumer Key (API Key)	dkm5NZhXmtyLdrif8PCcmIBkX
-# Consumer Secret (API Secret)	wOivaCX3qE0XFFuKEbK1aEwLtccrTlIKVTFUeoPpaUnNbCt4Gz
-
-# Access Token	799354686662787072-XDzKnFhMZzAfdPnUKU2JGHE2DW5zgvk
-# Access Token Secret	oRhBN2cuZ03Zqxz5lf50uAWRE1TxnRjZEJybXcD1nZYay
-
-consumer_key= "dkm5NZhXmtyLdrif8PCcmIBkX"
-consumer_secret= "wOivaCX3qE0XFFuKEbK1aEwLtccrTlIKVTFUeoPpaUnNbCt4Gz"
-access_token= "799354686662787072-XDzKnFhMZzAfdPnUKU2JGHE2DW5zgvk"
-access_token_secret= "oRhBN2cuZ03Zqxz5lf50uAWRE1TxnRjZEJybXcD1nZYay"
 
 
 # authotication 
